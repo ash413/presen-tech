e40c3048f717404b763d5174c04f92f2367baaa3
Vaishnavi Kadam <vaishnavi.kadam413@gmail.com>
2024-10-27 20:01:06 -0400

    frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+
 
 function App() {
+  const [file, setFile] = useState(null);
+  const [docType, setDocType] = useState("general");
+  const [generatedppt, setGeneratedppt] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleFileUpload = (file) => {
+    setFile(file);
+  }
+
+  const handleDocTypeChange = (e) => {
+    setDocType(e.target.value);
+  }
+
+  const handleSubmit = async () => {
+    if(!file){
+      alert('Please upload a file first!');
+      return;
+    }
+    setLoading(true);
+
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("docType", docType);
+
+    try {
+      const response = await fetch("http://localhost:5000/generate-ppt", {
+        method: "POST",
+        body: formData
+      });
+      const blob = await response.blob();
+      setGeneratedppt(URL.createObjectURL(blob));
+
+    } catch (error) {
+      console.log("Error generating PPT", error);
+    } finally{
+      setLoading(false);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
     </div>
   );
 }
